7163cf74f1b6584ff0ea8f4a8609e4f4327f43d9
aki <aki@mymail.com>
2024-09-21 11:15:47 +0900

    クライアントサイドのコードをLuaで書く: rpcnotify()が上手く動かない

---
+++ plugin/neovim-calculator.lua
@@ -0,0 +1,53 @@
+local calculatorJobLuaId = 0
+
+local bin = "/home/aki/nvim_calc_rs/target/release/nvim_calc_rs"
+
+local connect = function()
+	local id = initRpc()
+
+	if 0 == id then
+		print("calculator: cannot start rpc process")
+	elseif -1 == id then
+		print("calculator: cannot start rpc process")
+	else
+		print("initRpc(): Ok")
+		print("id = " .. id)
+		calculatorJobLuaId = id
+	end
+end
+
+--- initialize RPC
+initRpc = function()
+	if calculatorJobLuaId == 0 then
+		jobid = vim.fn.jobstart(bin, { prc = true })
+		return jobid
+	else
+		return calculatorJobLuaId
+	end
+end
+
+vim.api.nvim_create_user_command("AddL", function(ops)
+	local args = ops.fargs
+	if #args < 2 then
+		return
+	end
+	local p = tonumber(args[1])
+	local q = tonumber(args[2])
+
+	print("calculatorJobLuaId(in AddL) = " .. calculatorJobLuaId)
+
+	vim.rpcnotify(calculatorJobLuaId, "add", p, q)
+end, { nargs = "*" })
+
+vim.api.nvim_create_user_command("MulL", function(ops)
+	local args = ops.fargs
+	if #args < 2 then
+		return
+	end
+	local p = tonumber(args[1])
+	local q = tonumber(args[2])
+
+	vim.rpcnotify(calculatorJobLuaId, "multiply", p, q)
+end, { nargs = "*" })
+
+connect()
