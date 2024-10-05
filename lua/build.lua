ab3b72b9f06817c5db644d25fbaf7f4f620bca88
aki <aki@mymail.com>
2024-10-06 02:00:08 +0900

    gitの差分を見つける関数を追加し、非同期なビルド関数を削除した

--- lua/build.lua
+++ lua/build.lua
@@ -7,6 +7,17 @@ get_project_root_dir = function()
 	return plugin_root
 end
 
+local is_diff_in_src = function()
+	local diff = vim.fn.systemlist("git diff --name-only", get_project_root_dir())
+
+	for _, file_name in ipairs(diff) do
+		if file_name:match("Cargo.toml") or file_name:match(".*rs$") then
+			return true
+		end
+	end
+	return false
+end
+
 build_rust = function(is_debug, state)
 	local project_root_dir = get_project_root_dir()
 	local path_to_bin = project_root_dir .. "/target/release/nvim_calc_rs"
@@ -15,6 +26,8 @@ build_rust = function(is_debug, state)
 		print("projet_root_dir: " .. project_root_dir)
 		print("path_to_bin: " .. path_to_bin)
 	end
+
+	local _foo = is_diff_in_src()
 
 	if not vim.loop.fs_stat(path_to_bin) then
 		print("build rust binary ...")
@@ -33,117 +46,4 @@ build_rust = function(is_debug, state)
 	end
 
 	connect_to_bin(state)
-end
-
-build_rust_async = function(config, state)
-	local path_to_project = get_project_root_dir()
-	local path_to_bin = path_to_project .. "/target/release/nvim_calc_rs"
-	local job_id = 0
-
-	if not vim.loop.fs_stat(path_to_bin) then
-		if config.use_new_window then
-			vim.api.nvim_command("vnew")
-		end
-		vim.api.nvim_buf_set_lines(0, -1, -1, false, { "   build rust binary..." })
-
-		local build_cmd = "cargo build --release --manifest-path=" .. path_to_project .. "/Cargo.toml"
-		if config.is_debug then
-			print("build_cmd: " .. build_cmd)
-		end
-
-		job_id = vim.fn.jobstart(build_cmd, {
-			on_stderr = function(_, data, _)
-				if data then
-					-- Add the ping output to the current buffer in real time
-					vim.api.nvim_buf_set_lines(0, -1, -1, false, data)
-				end
-			end,
-			on_exit = function(_, code, _)
-				if code == 0 then
-					vim.api.nvim_buf_set_lines(0, -1, -1, false, { "   Build: Ok" })
-					connect_to_bin(state)
-				else
-					local error_message = string.format("   Build failed with exit code: %d", code)
-					vim.api.nvim_buf_set_lines(0, -1, -1, false, { error_message })
-				end
-			end,
-		})
-
-		vim.cmd("setlocal buftype=nofile")
-	else
-		if config.is_debug then
-			print("rust binary: Ok")
-		end
-		connect_to_bin(state)
-	end
-
-	return job_id
-end
-
-build_rust_async_window = function(config, state)
-	local path_to_project = get_project_root_dir()
-	local path_to_bin = path_to_project .. "/target/release/nvim_calc_rs"
-	local job_id = 0
-
-	if not vim.loop.fs_stat(path_to_bin) then
-		local build_cmd = "cargo build --release --manifest-path=" .. path_to_project .. "/Cargo.toml"
-		if config.is_debug then
-			print("build_cmd: " .. build_cmd)
-		end
-
-		-- window size and position
-		local width = 50
-		local height = 15
-		local col = math.floor((vim.o.columns - width) / 2)
-		local row = math.floor((vim.o.lines - height) / 2)
-
-		-- buffer for floating window
-		local buf = vim.api.nvim_create_buf(false, true)
-
-		-- options for floating window
-		local opts = {
-			style = "minimal",
-			relative = "editor",
-			width = width,
-			height = height,
-			col = col,
-			row = row,
-			border = "rounded",
-		}
-
-		-- generate floating window
-		vim.api.nvim_open_win(buf, true, opts)
-		vim.api.nvim_buf_set_lines(buf, 0, -1, false, { "   build rust binary ..." })
-
-		job_id = vim.fn.jobstart(build_cmd, {
-			on_stderr = function(_, data, _)
-				if data then
-					-- Get the current number of lines in the buffer
-					local current_lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
-
-					-- Combine current lines with new data
-					local new_lines = vim.list_extend(current_lines, data)
-
-					-- Set the updated lines back to the buffer
-					vim.api.nvim_buf_set_lines(buf, 0, -1, false, new_lines)
-				end
-			end,
-
-			on_exit = function(_, code, _)
-				if code == 0 then
-					connect_to_bin(state)
-				else
-					local error_message = string.format("   Build failed with exit code: %d", code)
-					print(error_message)
-				end
-			end,
-		})
-	else
-		if config.is_debug then
-			print("rust binary: Ok")
-		end
-		connect_to_bin(state)
-	end
-
-	return job_id
 end
