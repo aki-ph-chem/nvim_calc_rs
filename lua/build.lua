12ef34449aeecb3be0909c4fc4f1babf698c3715
aki <aki@mymail.com>
2024-10-06 15:31:41 +0900

    タイムスタンプの取得,ディレクトリ操作のための関数を追加

--- lua/build.lua
+++ lua/build.lua
@@ -7,7 +7,7 @@ get_project_root_dir = function()
 	return plugin_root
 end
 
-local is_diff_in_src = function()
+local is_diff_git_rust = function()
 	local diff = vim.fn.systemlist("git diff --name-only", get_project_root_dir())
 
 	for _, file_name in ipairs(diff) do
@@ -18,9 +18,46 @@ local is_diff_in_src = function()
 	return false
 end
 
+local get_time_stamp = function(path_to_file)
+	local stat = vim.loop.fs_stat(path_to_file)
+	return os.date("%Y-%m-%d %H:%M:%S", stat.mtime.sec)
+end
+
+local function traverse_dir(path, postfix_list)
+	local scandir = vim.loop.fs_scandir(path)
+
+	if scandir then
+		while true do
+			local name, typ = vim.loop.fs_scandir_next(scandir)
+			if not name then
+				break
+			end
+
+			local fullpath = path .. "/" .. name
+			for _, p in pairs(postfix_list) do
+				if fullpath:match(p) then
+					print(fullpath, typ)
+				end
+			end
+
+			if typ == "directory" then
+				traverse_dir(fullpath, postfix_list)
+			end
+		end
+	else
+		print("Failed to open directory: " .. path)
+	end
+end
+
 build_rust = function(is_debug, state)
 	local project_root_dir = get_project_root_dir()
 	local path_to_bin = project_root_dir .. "/target/release/nvim_calc_rs"
+
+	--[[
+	local postfix = { "%.rs", "Cargo.toml" }
+	traverse_dir(project_root_dir, postfix)
+	local res = {}
+        --]]
 
 	if is_debug then
 		print("projet_root_dir: " .. project_root_dir)
